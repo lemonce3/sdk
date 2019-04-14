f692ac1b31f18d077eb4146aac8f5cc8d11137a2
lichaozhy <lichao@or-change.cn>
2019-04-15 05:53:02 +0800

    [FIXED] config from

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-global.config = require('./config.json');
+const path = require('path');
+global.config = require(path.resolve('config.json'));
 
 exports.Master = require('./src/master');
 exports.utils = require('./src/uitls');
